68e8c2d901c4b3a955822fc240086cca96984942
Diana Grams <deegrams221@gmail.com>
2019-10-03 14:28:23 -0600

    setup HockeyCreationInjector and HockeyImpl

--- athlete/src/com/lambdaschool/solution/HockeyCreationInjector.java
+++ athlete/src/com/lambdaschool/solution/HockeyCreationInjector.java
@@ -1,6 +1,11 @@
 package com.lambdaschool.solution;
 
-public class HockeyCreationInjector
+public class HockeyCreationInjector implements AthleteCreationInjector
 {
-
+    // generate override
+    @Override
+    public Processor getProcess()
+    {
+        return new MyAthlete(new HockeyImpl());
+    }
 }
--- athlete/src/com/lambdaschool/solution/HockeyImpl.java
+++ athlete/src/com/lambdaschool/solution/HockeyImpl.java
@@ -1,4 +1,12 @@
 package com.lambdaschool.solution;
 
-public class HockeyImpl {
+public class HockeyImpl implements AthleteService
+{
+    // generate override
+    @Override
+    public void displayAthlete(String athlete)
+    {
+        // sout
+        System.out.println(athlete);
+    }
 }
