8b68c73d18c1548a7c963e6d5c9380e3fc2a8d6c
Diana Grams <deegrams221@gmail.com>
2019-10-03 14:31:29 -0600

    setup RugbyCreationInjector and RugbyImpl

--- athlete/src/com/lambdaschool/solution/RugbyCreationInjector.java
+++ athlete/src/com/lambdaschool/solution/RugbyCreationInjector.java
@@ -1,4 +1,11 @@
 package com.lambdaschool.solution;
 
-public class RugbyCreationInjector {
+public class RugbyCreationInjector implements AthleteCreationInjector
+{
+    // generate override
+    @Override
+    public Processor getProcess()
+    {
+        return new MyAthlete(new RugbyImpl());
+    }
 }
--- athlete/src/com/lambdaschool/solution/RugbyImpl.java
+++ athlete/src/com/lambdaschool/solution/RugbyImpl.java
@@ -1,4 +1,12 @@
 package com.lambdaschool.solution;
 
-public class RugbyImpl {
+public class RugbyImpl implements AthleteService
+{
+    // generate override
+    @Override
+    public void displayAthlete(String athlete)
+    {
+        //sout
+        System.out.println(athlete);
+    }
 }
